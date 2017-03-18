6aeeaf66bb509e1a57c77dc49bae3eb64e2ded42
florinn <florin.nitoi@gmail.com>
2017-03-18 15:15:29 +0200

    add linting

--- src/app.js
+++ src/app.js
@@ -1,19 +1,34 @@
-import {app, BrowserWindow} from 'electron';
-import {enableLiveReload} from 'electron-compile';
+// Modules to control application life and to create native browser window.
+import { app, BrowserWindow } from 'electron';
+import { enableLiveReload } from 'electron-compile';
 
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
 let mainWindow = null;
 
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  app.quit();
+  if (process.platform != 'darwin')
+    app.quit();
 });
 
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
 app.on('ready', () => {
-  enableLiveReload({strategy: 'react-hmr'});
-  mainWindow = new BrowserWindow({
-    width: 700,
-    height: 760,
-    titleBarStyle: 'hidden-inset'
-  });
+  // Enable React HMR
+  enableLiveReload({ strategy: 'react-hmr' });
 
+  // Create the browser window.
+  mainWindow = new BrowserWindow({ width: 800, height: 760 });
+
+  // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 });
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,7 +5,7 @@ import { AppContainer } from 'react-hot-loader';
 const render = () => {
   const Main = require('./components/main').Main;
   ReactDOM.render(<AppContainer><Main/></AppContainer>, document.getElementById('app'));
-}
+};
 
 render();
 if (module.hot) { module.hot.accept(render); }
